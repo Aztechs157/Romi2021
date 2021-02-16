b95022fd8a658e530817d0aa31a6f7d231d05aca
Unspecified <Unspecified>
2021-02-16 18:36:07 -0500

    Code cleanup

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -8,24 +8,18 @@ import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutonomousDistance extends SequentialCommandGroup {
-  /**
-   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
-   * turn around and drive back.
-   *
-   * @param drivetrain The drivetrain subsystem on which this command will run
-   */
-  public AutonomousDistance(Drivetrain drivetrain) {
-    addCommands(
-        new DriveDistance(0.5, 17, drivetrain),
-        new TurnDegrees(-0.5, 80, drivetrain),
-        new DriveDistance(0.5, 17, drivetrain),
-        new TurnDegrees(-0.5, 90, drivetrain),
-        new DriveDistance(0.5,14, drivetrain),
-        new TurnDegrees(-0.5, 15, drivetrain),
-        new DriveDistance(0.5, 12, drivetrain),
-        new TurnDegrees(0.5, 115, drivetrain),
-        new DriveDistance(0.5, 19, drivetrain),
-        new TurnDegrees(0.5, 80, drivetrain),
-        new DriveDistance(.5, 19, drivetrain));
-  }
+    /**
+     * Creates a new Autonomous Drive based on distance. This will drive out for a
+     * specified distance, turn around and drive back.
+     *
+     * @param drivetrain The drivetrain subsystem on which this command will run
+     */
+    public AutonomousDistance(Drivetrain drivetrain) {
+        addCommands(new DriveDistance(0.5, 17, drivetrain), new TurnDegrees(-0.5, 80, drivetrain),
+                new DriveDistance(0.5, 17, drivetrain), new TurnDegrees(-0.5, 90, drivetrain),
+                new DriveDistance(0.5, 14, drivetrain), new TurnDegrees(-0.5, 15, drivetrain),
+                new DriveDistance(0.5, 12, drivetrain), new TurnDegrees(0.5, 115, drivetrain),
+                new DriveDistance(0.5, 19, drivetrain), new TurnDegrees(0.5, 80, drivetrain),
+                new DriveDistance(.5, 19, drivetrain));
+    }
 }
